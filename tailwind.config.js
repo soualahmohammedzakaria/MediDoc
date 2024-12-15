8e7c1dc0ed5afcff00be7ca9e369ed7102df7691
Azzouz Abdellah Aimen <ma_azzouz@esi.dz>
2024-12-15 15:00:05 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       colors: {
-        primary: '#0B8FAC', // custom primary color
+        "custom-blue": "#0B8FAC",
+        "custom-teal": "#7BC1B7",
+        "custom-light-teal": "#D2EBE7",
+        "custom-sky": "#1995B0",
+        "custom-orange": "#F89603",
+        "custom-dark-blue": "#13375B",
+        "custom-green": "#129820",
+        "custom-red": "#F30000",
+        "custom-gray-dark": "#5F5F60",
+        "custom-gray-medium": "#4F4E69",
+        "custom-gray-light": "#858585",
       },
-      fontFamily: {
-        sans: ['"Red Hat Display"', 'sans-serif']
-      }
     },
   },
   plugins: [],
-}
+};
