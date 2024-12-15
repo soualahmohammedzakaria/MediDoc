eedac8a8bc7065770750ba447f3bfb850b8258a8
Mohamed <mm_maraf@esi.dz>
2024-12-15 14:23:49 +0100

    tailwind config updated with colors chart

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue": "#0B8FAC",
+        "custom-teal": "#7BC1B7",
+        "custom-light-teal": "#D2EBE7",
+        "custom-sky": "#1995B0",
+        "custom-orange": "#F89603",
+        "custom-dark-blue": "#13375B",
+        "custom-green": "#129820",
+        "custom-red": "#F30000",
+        "custom-gray-dark": "#5F5F60",
+        "custom-gray-medium": "#4F4E69",
+        "custom-gray-light": "#858585",
+      },
+    },
   },
   plugins: [],
-}
-
+};
